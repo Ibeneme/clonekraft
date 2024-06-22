291615b3ebaebfbd152bc18e0d32ca6cf6a04794
Ibeneme <macbook@Ibeneme-Ikennas.local>
2024-06-22 15:56:17 +0100

    update

--- src/Redux/Baseurl/Baseurl.js
+++ src/Redux/Baseurl/Baseurl.js
@@ -1,2 +1,2 @@
-//export const baseApiUrl = `https://clone-kraft-s-server.onrender.com`;
-export const baseApiUrl = `http://localhost:3003`;
+export const baseApiUrl = `https://clone-kraft-s-server.onrender.com`;
+//export const baseApiUrl = `http://localhost:3003`;
