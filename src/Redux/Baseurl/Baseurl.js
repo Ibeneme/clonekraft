d32a04eaac539622b8cfd9e92ca4ac61b375b5af
Ibeneme <ibenemeikenna96@gmail.com>
2025-02-16 23:58:02 +0100

    updates

--- src/Redux/Baseurl/Baseurl.js
+++ src/Redux/Baseurl/Baseurl.js
@@ -1,3 +1,3 @@
-//export const baseApiUrl = `https://clone-kraft-s-server.onrender.com`;
+export const baseApiUrl = `https://clone-kraft-s-server-rjx3.onrender.com`;
 
-export const baseApiUrl = `http://localhost:3003`;
+//export const baseApiUrl = `http://localhost:3003`;
